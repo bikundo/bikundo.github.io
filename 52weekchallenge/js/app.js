e59a266c9e839d817b6bec721719180772ff38bd
bix <binmonk@gmail.com>
2017-01-04 10:45:21 +0300

    add 52 week challenge folder

---
+++ 52weekchallenge/js/app.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        starting: 100,
+        message: 'Hello Vue!',
+    },
+    computed: {
+        computedTotals: function () {
+            weeks = new Array();
+            weeks[0] = [];
+            weeks[0]['deposit'] = this.starting;
+            weeks[0]['total'] = this.starting;
+            weeks[0]['weekNumber'] = 1;
+            for (i = 1; i < 52; i++) {
+                if (i === 1) {
+                    var previousIndex = 0;
+                }
+                else {
+                    var previousIndex = i - 1;
+                }
+                weeks[i] = [];
+                weeks[i]['weekNumber'] = i + 1;
+                weeks[i]['deposit'] = this.starting + weeks[previousIndex]['deposit'];
+                weeks[i]['total'] = this.starting + weeks[previousIndex]['total'] + weeks[previousIndex]['deposit'];
+            }
+            return weeks;
+        },
+        totalSaved: function () {
+            var lastMonth = this.computedTotals.slice(-1).pop();
+            return lastMonth['total'].toLocaleString();
+        }
+    },
+    mounted: function () {
+        console.log(this.computedTotals);
+    }
+})
